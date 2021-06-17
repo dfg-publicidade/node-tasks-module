e0c8d1df26f636566ce1f4e293c7542f7962a8a2
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-06-17 17:07:17 -0300

    Adição das classes Task e TaskManager

--- dist/tasks/taskManager.d.ts
+++ dist/tasks/taskManager.d.ts
@@ -1,5 +1,9 @@
 import Task from '../interfaces/task';
 declare abstract class DefaultTaskManager {
+    abstract init(): Promise<void>;
+    abstract cancelRunningTasks(): Promise<void>;
+    abstract generateDefaultTasks(): Promise<void>;
+    abstract getNext(): Promise<Task>;
     abstract update(task: Task, data: any): Promise<Task>;
     abstract sendToSolved(task: Task): Promise<void>;
     abstract delete(task: Task): Promise<void>;
--- src/tasks/taskManager.ts
+++ src/tasks/taskManager.ts
@@ -2,6 +2,10 @@ import Task from '../interfaces/task';
 
 /* Module */
 abstract class DefaultTaskManager {
+    public abstract init(): Promise<void>;
+    public abstract cancelRunningTasks(): Promise<void>;
+    public abstract generateDefaultTasks(): Promise<void>;
+    public abstract getNext(): Promise<Task>;
     public abstract update(task: Task, data: any): Promise<Task>;
     public abstract sendToSolved(task: Task): Promise<void>;
     public abstract delete(task: Task): Promise<void>;
