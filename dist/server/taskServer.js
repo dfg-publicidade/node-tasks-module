4cbddcfd553b35c13f93a5aacd6ef0a9d218798e
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2022-01-11 16:51:28 -0300

    Atualização de dependências 11/01/2022 16:04:28

--- dist/server/taskServer.js
+++ dist/server/taskServer.js
@@ -7,7 +7,7 @@ const cron_1 = require("cron");
 const debug_1 = __importDefault(require("debug"));
 const runner_1 = __importDefault(require("../tasks/runner"));
 /* Module */
-const debug = debug_1.default('module:task-server');
+const debug = (0, debug_1.default)('module:task-server');
 class TaskServer {
     constructor(app, taskManager) {
         debug('Building task server');
